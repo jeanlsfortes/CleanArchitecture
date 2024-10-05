50c49f320057bc69b70551cef7a45a902c2ec49d
jeanlsfortes <jeanlucaseh10@gmail.com>
2024-10-05 12:43:31 -0400

    fix: defining blog entity

---
+++ CleanArchitecture.Domain/Enitites/Blog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Enitites
+{
+    public sealed class Blog
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Author { get; private set; }
+        public string? ImageUrl { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+        public DateTime? DeletedAt { get; private set; }
+
+        public Blog(string name, string description, string author)
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            Name = name;
+            Description = description;
+            Author = author;
+        }
+
+        public void Update(string name, string description, string imageUrl)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void Delete()
+        {
+            DeletedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ CleanArchitecture.Domain/Interface/IBlogRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Enitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interface
+{
+    public interface IBlogRepository
+    {
+        Task<List<Blog>> GetAllAsync();
+        Task<Blog> GetByIdAsync(Guid id);
+        Task<Blog> CreateAsync(Blog blog);
+        Task<Guid> UpdateAsync(Guid id, Blog blog);
+        Task<Guid> DeleteAsync(Guid id);
+    }
+}
